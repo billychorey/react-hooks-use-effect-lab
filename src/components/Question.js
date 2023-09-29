1db2d5dbcded5bd7cc09418c0bd96da9b6f3ca2a
Billy <billychorey@gmail.com>
2023-09-29 13:34:03 -0400

    react hooks use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+   useEffect(() => {
+    const timerID = setInterval(() => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime === 0) {
+          setTimeRemaining(10);
+          onAnswered(false);
+        } else {
+          return prevTime - 1;
+        }
+      });
+    }, 1000);
+
+    // Cleanup function to clear the timer on unmount
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [onAnswered]);
+
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
